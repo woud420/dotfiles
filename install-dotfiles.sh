7b5a3a6c066a550e9dafdc3fd0fa109998b44f49
Jean-Michel Bouchard <jim@polarcoordinates.org>
2014-12-05 16:33:41 -0500

    Create install-dotfiles.sh

---
+++ install-dotfiles.sh
@@ -0,0 +1,6 @@
+# GDB and VIM integration
+Conque GDB
+
+# Install fonts for powerline
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
