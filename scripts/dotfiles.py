7634a13c7c162da31f3cc47fa9b496ae247d3efe
Jean-Michel Bouchard <jim@polarcoordinates.org>
2026-05-25 13:54:41 -0400

    Track Arch Sway config

--- scripts/dotfiles.py
+++ scripts/dotfiles.py
@@ -95,6 +95,10 @@ def managed_files(machine: dict) -> list[ManagedFile]:
 
     add_file(entries, "common/htop/htoprc", "~/.config/htop/htoprc", "htop")
 
+    if os_name in {"arch"}:
+        add_file(entries, "linux/arch/.config/sway/config", "~/.config/sway/config", "sway")
+        add_glob(entries, "linux/arch/.config/sway/assets/*", "~/.config/sway/assets", "sway")
+
     add_file(entries, ".vim/vimrc", "~/.vim/vimrc", "vim")
     add_file(entries, ".vim/plugins.vim", "~/.vim/plugins.vim", "vim")
     add_file(entries, ".vim/mappings.vim", "~/.vim/mappings.vim", "vim")
